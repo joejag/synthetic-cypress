fc79208015c42b6db0fb79a7d19e4826292682d1
Joe Wright <joe@joejag.com>
2020-11-17 16:23:46 +0000

    Update e2e-tests.js

--- e2e-tests.js
+++ e2e-tests.js
@@ -1,58 +1,99 @@
-const cypress = require('cypress')
-const cron = require('node-cron')
-const serveIndex = require('serve-index')
-const express = require('express')
+const cypress = require("cypress");
+const cron = require("node-cron");
+const serveIndex = require("serve-index");
+const express = require("express");
+const promBundle = require("express-prom-bundle");
+const client = require("prom-client");
+const { merge } = require("mochawesome-merge");
+const generator = require("mochawesome-report-generator");
+const fse = require("fs-extra");
 
-const app = express()
+const app = express();
+const metricsMiddleware = promBundle({ includeMethod: true });
+app.use(metricsMiddleware);
 
-let currentSummary = {}
-app.get('/', (req, res) => {
-  res.setHeader('content-type', 'application/json')
+let currentSummary = {};
+app.get("/", (req, res) => {
+  res.setHeader("content-type", "application/json");
   const result = {
-    startedTestsAt: currentSummary.startedTestsAt,
-    endedTestsAt: currentSummary.endedTestsAt,
-    totalDuration: currentSummary.totalDuration,
-    totalSuites: currentSummary.totalSuites,
-    totalTests: currentSummary.totalTests,
-    totalFailed: currentSummary.totalFailed,
-    totalPassed: currentSummary.totalPassed,
-    totalPending: currentSummary.totalPending,
-    totalSkipped: currentSummary.totalSkipped
-  }
-  res.send(JSON.stringify(result, null, 4))
-})
+    lastRun: {
+      startedTestsAt: currentSummary.startedTestsAt,
+      endedTestsAt: currentSummary.endedTestsAt,
+      totalDuration: currentSummary.totalDuration,
+      totalSuites: currentSummary.totalSuites,
+      totalTests: currentSummary.totalTests,
+      totalFailed: currentSummary.totalFailed,
+      totalPassed: currentSummary.totalPassed,
+      totalPending: currentSummary.totalPending,
+      totalSkipped: currentSummary.totalSkipped,
+    },
+    statusPageLink: "http://localhost:3000/status/mochawesome.html",
+    videosLink: "http://localhost:3000/videos",
+    screenshotsLink: "http://localhost:3000/screenshots",
+  };
+  res.send(JSON.stringify(result, null, 4));
+});
 
-app.get('/debug', (req, res) => {
-  res.setHeader('content-type', 'application/json')
-  res.send(JSON.stringify(currentSummary, null, 4))
-})
+app.get("/debug", (req, res) => {
+  res.setHeader("content-type", "application/json");
+  res.send(JSON.stringify(currentSummary, null, 4));
+});
 
-const PORT = 3000
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Synthetic Cypress listening at http://localhost:${PORT}`)
-})
+  console.log(`Synthetic Cypress listening at http://localhost:${PORT}`);
+});
 
 // Enable directory lists of videos
-app.use('/videos', serveIndex(__dirname + '/cypress/videos'))
-app.use('/videos', express.static(__dirname + "/cypress/videos"))
+app.use("/videos", serveIndex(__dirname + "/cypress/videos"));
+app.use("/videos", express.static(__dirname + "/cypress/videos"));
+app.use("/screenshots", serveIndex(__dirname + "/cypress/screenshots"));
+app.use("/screenshots", express.static(__dirname + "/cypress/screenshots"));
+app.use("/status", serveIndex(__dirname + "/mochawesome-report"));
+app.use("/status", express.static(__dirname + "/mochawesome-report"));
 
 // Run every minute
-cron.schedule('* * * * *', function() {
-  console.log('Running tests...')
-  runTests((summary) => {
-    currentSummary = summary
-  })
-})
+cron.schedule("* * * * *", function () {
+  console.log("Running tests...");
+
+  fse.remove("cypress/results").then(() => {
+    runTests((summary) => {
+      currentSummary = summary;
+      fse
+        .remove("mochawesome-report")
+        .then(() => merge({ files: ["cypress/results/*.json"] }))
+        .then((r) => generator.create(r));
+
+      // set guages
+    });
+  });
+});
 
 const runTests = (cb) => {
-  cypress.run({
+  cypress
+    .run({
       quiet: true,
       config: {
         video: true,
       },
-      specs: './cypress/integration/*-spec.js'
+      specs: "./cypress/integration/*-spec.js",
     })
     .then((runsResults) => {
-      cb(runsResults)
-    })
-}
+      cb(runsResults);
+    });
+};
+
+const scenarioStatusGauge = new client.Gauge({
+  name: "scenario_status",
+  help: "Indicates if the tests are passing(1), failing(-1) or not run(0)",
+  labelNames: ["scenario"],
+});
+scenarioStatusGauge.set({ scenario: "rollup" }, 1);
+scenarioStatusGauge.set({ scenario: "test name" }, 1);
+
+const scenarioTimingGuage = new client.Gauge({
+  name: "scenario_timing",
+  help: "How long a scenario took to run in seconds",
+  labelNames: ["scenario"],
+});
+scenarioTimingGuage.set({ scenario: "test name" }, 123);
